7511f50d1410270166f900d2d3703b17ded1952b
Hoang The Toan <toanht15@gmail.com>
2015-10-28 10:29:59 +0700

    add password resets and email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates(:name, presence: true, length: {maximum: 50})
@@ -22,22 +22,34 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
-# Activates an account.
-def activate
-  update_attribute(:activated,    true)
-  update_attribute(:activated_at, Time.zone.now)
-end
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
 
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-
 
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
@@ -50,6 +62,11 @@ end
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
   # Converts email to all lower-case.
   def downcase_email
