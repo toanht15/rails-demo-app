b8cb180491887bab0d0660e54f526900082a9d0b
Hoang The Toan <toanht15@gmail.com>
2015-10-29 16:20:38 +0700

    fix avatar size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,11 @@ module UsersHelper
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
+
+	def avatar_url(user, size)
+		gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+		#gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+		image_tag(gravatar_url,alt: user.name, class: "gravatar")       
+    end
 end
